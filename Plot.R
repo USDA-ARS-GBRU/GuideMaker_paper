f4ebe1afa5f15ccd00f8c2a159872f3a46685ff8
ravin.poudel <admin@admins-Air.hsd1.ct.comcast.net>
2020-08-31 12:44:34 -0400

    large gbk tested

--- Plot.R
+++ Plot.R
@@ -36,7 +36,7 @@ p3 <- ggplot(summarystat, aes(x=threads, y=process_sec, colour=Genome, group=Gen
   expand_limits(y=0) + # Expand y range
   theme_bw() +
   theme(legend.justification=c(1,0),
-        legend.position=c(0.48,0.75)) # Position legend (left-right, top-bottom)
+        legend.position=c(0.48,0.70)) # Position legend (left-right, top-bottom)
 
  
 p4 <- p3 + facet_grid(. ~ Processor)
@@ -49,7 +49,7 @@ p5 <- p4 + theme(axis.text.x = element_text(color = "black", size = 10, angle = 
   theme(legend.title = element_text(face = "bold"))
 
 p5 + scale_x_continuous(breaks = c(0, 4, 8, 12, 16, 20, 24, 28, 32)) +
-  scale_y_continuous(breaks = seq(0, 1200, 100))
+  scale_y_continuous(breaks = seq(0, 3500, 250))
   
 
 # Save the plot
@@ -63,3 +63,141 @@ print("Plot saved as AVX2_Performance_Graph.pdf")
 
 ##
 file.remove("Rplots.pdf")
+
+
+
+
+
+#######fold change in the run time
+
+AVX2_1 <- summarystat %>%
+  filter (threads==1 & Processor =="AVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+AVX2_2 <-summarystat %>%
+  filter (threads==2 & Processor =="AVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+AVX2_4 <-summarystat %>%
+  filter (threads==4 & Processor =="AVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+AVX2_8 <-summarystat %>%
+  filter (threads==8 & Processor =="AVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+AVX2_16 <-summarystat %>%
+  filter (threads==16 & Processor =="AVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+AVX2_32 <-summarystat %>%
+  filter (threads==32 & Processor =="AVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+########### non AVX2 ###########
+
+nonAVX2_1 <- summarystat %>%
+  filter (threads==1 & Processor =="nonAVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+nonAVX2_2 <-summarystat %>%
+  filter (threads==2 & Processor =="nonAVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+nonAVX2_4 <-summarystat %>%
+  filter (threads==4 & Processor =="nonAVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+nonAVX2_8 <-summarystat %>%
+  filter (threads==8 & Processor =="nonAVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+nonAVX2_16 <-summarystat %>%
+  filter (threads==16 & Processor =="nonAVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+nonAVX2_32 <-summarystat %>%
+  filter (threads==32 & Processor =="nonAVX2") %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+#######
+
+
+
+AVX2_ratio_1_2_4_8_16 <- c(AVX2_1/AVX2_2, AVX2_2/AVX2_4, AVX2_4/AVX2_16,AVX2_16/AVX2_32)
+AVX2_ratio_1_2_4_8_16
+
+nonAVX2_ratio_1_2_4_8_16 <- c(nonAVX2_1/nonAVX2_2, nonAVX2_2/nonAVX2_4, nonAVX2_4/nonAVX2_16,nonAVX2_16/nonAVX2_32)
+nonAVX2_ratio_1_2_4_8_16
+
+
+data.frame(processors=c(1, 2, 4, 8, 16, 32), 
+           Mean_run_time_AVX2 = c(AVX2_1, AVX2_2, AVX2_4, AVX2_8, AVX2_16, AVX2_32),
+           Mean_run_time_nonAVX2 = c(nonAVX2_1, nonAVX2_2, nonAVX2_4, nonAVX2_8, nonAVX2_16, nonAVX2_32))
+
+
+
+
+
+#########
+processor_1 <- summarystat %>%
+  filter (threads==1 ) %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+processor_2 <-summarystat %>%
+  filter (threads==2 ) %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+processor_4 <-summarystat %>%
+  filter (threads==4 ) %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+processor_8 <-summarystat %>%
+  filter (threads==8 ) %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+processor_16 <-summarystat %>%
+  filter (threads==16 ) %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+processor_32 <-summarystat %>%
+  filter (threads==32 ) %>%
+  summarize(mean=mean(process_sec)) %>% pull()
+
+
+ratio_1_2_4_8_16 <- c(processor_1/processor_2,
+                              processor_2/ processor_4,
+                              processor_4/ processor_8,
+                              processor_8/ processor_16,
+                      processor_16/ processor_32)
+
+
+ratio_1_2_4_8_16 
+
+
+
+
+require(vcd)
+require(MASS)
+
+# data generation
+ps <- summarystat %>%
+  pull(process_sec)
+  
+# estimate the parameters
+fit1 <- fitdistr(ps, "exponential") 
+
+# plot a graph
+hist(ps, freq = FALSE, breaks = 5, xlim = c(0, quantile(ps, 0.99)))
+curve(dexp(x, rate = fit1$estimate), from = 0, col = "red", add = TRUE)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
